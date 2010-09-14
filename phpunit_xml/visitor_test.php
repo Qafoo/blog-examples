bbbccbf526b9e0ce190e3a4b3f58aec32fb481be
Tobias Schlitt <toby@php.net>
2010-09-14 14:57:46 +0200

    - Fixed: Generation of expected XML in naive test.

--- phpunit_xml/visitor_test.php
+++ phpunit_xml/visitor_test.php
@@ -54,8 +54,8 @@ class qaVisitorTest extends qaXmlTestCase
         );
         $expPersonElem->appendChild(
             $expDom->createElement(
-                'LastName',
-                $person->getLastName()
+                'FirstName',
+                $person->getFirstName()
             )
         );
         $expPersonElem->appendChild(
