311b3d9cc2630615e8874c7955927dbcb9c6de30
Tobias Schlitt <toby@php.net>
2010-09-14 15:01:29 +0200

    - Implemented: Finished naive test case. - Implemented: assertSelect() and assertTag() test cases.

--- phpunit_xml/visitor_test.php
+++ phpunit_xml/visitor_test.php
@@ -70,8 +70,125 @@ class qaVisitorTest extends qaXmlTestCase
                 $person->getDateOfBirth()->format( 'Y-m-d' )
             )
         );
+
+        $visitor = new qaPersonVisitor( $this->rootElement );
+        $visitor->visitPerson( $person );
+
+        $this->assertEquals(
+            $expDom,
+            $this->getDomDocument()
+        );
     }
 
+    public function testVisitPersonSelectCSS()
+    {
+        $person = $this->getPersonFixture();
+
+        $visitor = new qaPersonVisitor( $this->rootElement );
+        $visitor->visitPerson( $person );
+
+        $this->assertSelectCount(
+            'Person',
+            1,
+            $this->getDomDocument(),
+            'Invalid number of Person elements',
+            false
+        );
+
+        // Broken CSS parser
+        $this->assertSelectEquals(
+            'FirstName',
+            $person->getFirstName(),
+            1,
+            $this->getDomDocument(),
+            'Invalid content of FirstName element',
+            false
+        );
+        $this->assertSelectEquals(
+            'LastName',
+            $person->getLastName(),
+            1,
+            $this->getDomDocument(),
+            'Invalid content of LastName element',
+            false
+        );
+        $this->assertSelectEquals(
+            'Gender',
+            $person->getGender(),
+            1,
+            $this->getDomDocument(),
+            'Invalid content of Gender element',
+            false
+        );
+        $this->assertSelectEquals(
+            'DateOfBirth',
+            $person->getDateOfBirth()->format( 'Y-m-d' ),
+            1,
+            $this->getDomDocument(),
+            'Invalid content of DateOfBirth element',
+            false
+        );
+    }
+
+    public function testVisitPersonTag()
+    {
+        $person = $this->getPersonFixture();
+
+        $visitor = new qaPersonVisitor( $this->rootElement );
+        $visitor->visitPerson( $person );
+        
+        $this->assertTag(
+            array(
+                'tag' => 'Person',
+                'child' => array(
+                    'tag'     => 'LastName',
+                    'content' => $person->getLastName(),
+                ),
+            ),
+            $this->getDomDocument(),
+            'Incorrect LastName tag',
+            false
+        );
+        
+        $this->assertTag(
+            array(
+                'tag' => 'Person',
+                'child' => array(
+                    'tag'     => 'FirstName',
+                    'content' => $person->getFirstName(),
+                ),
+            ),
+            $this->getDomDocument(),
+            'Incorrect FirstName tag',
+            false
+        );
+        
+        $this->assertTag(
+            array(
+                'tag' => 'Person',
+                'child' => array(
+                    'tag'     => 'Gender',
+                    'content' => (string) $person->getGender(),
+                ),
+            ),
+            $this->getDomDocument(),
+            'Incorrect Gender tag',
+            false
+        );
+        
+        $this->assertTag(
+            array(
+                'tag' => 'Person',
+                'child' => array(
+                    'tag'     => 'DateOfBirth',
+                    'content' => $person->getDateOfBirth()->format( 'Y-m-d' ),
+                ),
+            ),
+            $this->getDomDocument(),
+            'Incorrect DateOfBirth tag',
+            false
+        );
+    }
 
 }
 
